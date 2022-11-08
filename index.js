3bb27b984216a7636bc07351121ec1fa6d79bd24
Justin Chung <jdchung@scu.edu>
2022-11-07 18:15:38 -0800

    Deploy slash commands

--- index.js
+++ index.js
@@ -28,3 +28,36 @@ client.player = new Player(client, {
 });
 
 let commands = [];
+
+// Loads slash commands, only looks at files that end with .js
+const slashFiles = fs
+  .readdirSync("./slash")
+  .filter((file) => file.endsWith(".js"));
+for (const file of slashFiles) {
+  const slashcmd = require(`./slash/${file}`);
+  client.slashcommands.set(slashcmd.data.name, slashcmd);
+  if (LOAD_SLASH) commands.push(slashcmd.data.toJSON());
+}
+
+// Deploy slash commands using Rest API
+if (LOAD_SLASH) {
+  const rest = new REST({ version: "9" }).setToken(TOKEN);
+  console.log("Slash commands deploying");
+  // Generates a URL using the Client ID and Guild ID in order to deploy commands
+  rest
+    .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    })
+    .then(() => {
+      console.log("Deploy Success");
+      // Exit with success
+      process.exit(0);
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+        // Exit with failure
+        process.exit(1);
+      }
+    });
+}
