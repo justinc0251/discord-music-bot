723b6bb66561ecf9753703ea4e7e4fc6e5d6de2f
Justin Chung <jdchung@scu.edu>
2022-11-07 19:07:36 -0800

    Run queue command and handle errors

--- slash/queue.js
+++ slash/queue.js
@@ -12,4 +12,21 @@ module.exports = {
         .setDescription("Page number of Queue")
         .setMinValue(1)
     ),
+
+  run: async ({ client, interaction }) => {
+    // Get queue object
+    const queue = client.player.getQueue(interaction.guildId);
+    if (!queue || !queue.playing) {
+      return await interaction.editReply("No songs!");
+    }
+
+    // 10 Songs per page or 1 page at max if less than 10 songs in queue
+    const totalPages = Math.ceil(queue.tracks.length / 10) || 1;
+    // Specify page
+    // -1 to find first item in array
+    const page = (interaction.options.getNumber("page") || 1) - 1;
+
+    if (page > totalPages)
+      return await interaction.editReply(`There are only ${totalPages} pages!`);
+  },
 };
