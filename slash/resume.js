218eb2816b9766fc9f5494e115df42e6d12a5eae
Justin Chung <jdchung@scu.edu>
2022-11-07 23:46:47 -0800

    Build resume command

---
+++ slash/resume.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  // Build resume command
+  data: new SlashCommandBuilder()
+    .setName("resume")
+    .setDescription("Resumes music"),
+  run: async ({ client, interaction }) => {
+    const queue = client.player.getQueue(interaction.guildId);
+
+    if (!queue) return await interaction.editReply("No songs!");
+    queue.setPaused(false);
+    await interaction.editReply("Resumed!");
+  },
+};
