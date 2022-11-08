57cf5982be84be8fa3c5cf495b0f5dba397e687a
Justin Chung <jdchung@scu.edu>
2022-11-07 23:48:44 -0800

    Implement skip command

---
+++ slash/skip.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  // Build skip command
+  data: new SlashCommandBuilder().setName("skip").setDescription("Skips song"),
+  run: async ({ client, interaction }) => {
+    const queue = client.player.getQueue(interaction.guildId);
+
+    if (!queue) return await interaction.editReply("No songs!");
+    // Skips current song
+    const currentSong = queue.current;
+    queue.skip();
+    await interaction.editReply({
+      embeds: [
+        new MessageEmbed()
+          .setDescription(`${currentSong.title} skipped!`)
+          .setThumbnail(currentSong.thumbnail),
+      ],
+    });
+  },
+};
