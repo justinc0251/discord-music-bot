346f544446f051a4b398fd74bbc7480938ec6f38
Justin Chung <jdchung@scu.edu>
2022-11-07 18:43:08 -0800

    Execute song URL subcommand

--- slash/play.js
+++ slash/play.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const { QueryType } = require("discord-player");
 
 module.exports = {
   // Build play command
@@ -38,4 +40,42 @@ module.exports = {
             .setRequired(true)
         )
     ),
+  // Execute Code
+  run: async ({ client, interaction }) => {
+    // User must be in voice channel to call in bot
+    if (!interaction.member.voice.channel)
+      return interaction.editReply("Not in Voice Channel!");
+
+    // Create queue from client
+    const queue = await client.player.createQueue(interaction.guild);
+    // If no queue connection, bot will connect to the user's channel
+    if (!queue.connection)
+      await queue.connect(interaction.member.voice.channel);
+
+    let embed = new MessageEmbed();
+
+    // Look at requested subcommand
+    if (interaction.options.getSubcommand() === "song") {
+      let url = interaction.options.getString("url");
+      // Search on YouTube
+      const result = await client.player.search(url, {
+        requestedBy: interaction.player,
+        searchEngine: QueryType.YOUTUBE_VIDEO,
+      });
+      // Song not found in array
+      if (result.tracks.length === 0)
+        return interaction.editReply("Song not found!");
+
+      const song = result.tracks[0];
+      // Add song to queue
+      await queue.addTrack(song);
+      // Specify what we added
+      embed
+        .setDescription(`**[${song.title}](${song.url})** is added to Queue!`)
+        .setThumbnail(song.thumbnail)
+        .setFooter({ text: `Duration: ${song.duration}` });
+    } else if (interaction.options.getSubcommand() === "playlist") {
+    } else if (interaction.options.getSubcommand() === "search") {
+    }
+  },
 };
